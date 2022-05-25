6882d881037bd023a36110d448175c7f38d9b224
sinan <sinanalagoz74@gmail.com>
2022-05-25 19:33:18 +0300

    21

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Business Card'),
     );
   }
 }
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         color: Colors.teal,
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
 
               CircleAvatar(
@@ -51,9 +51,100 @@ class _MyHomePageState extends State<MyHomePage> {
                 backgroundImage: AssetImage("images/biyometrik.jpg"),
 
               ),
-              Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text("Sinan ALAGÖZ",style: TextStyle(fontFamily: 'WaterBrush',fontSize: 20),))
+
+
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text("Sinan ALAGÖZ",style: TextStyle(fontFamily: 'Shojumaru',
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                    child: Divider(
+                      color: Colors.teal.shade50,
+                    ),
+                  ),
+                  Text("Flutter Developer",
+                    style: TextStyle(color: Colors.white,
+                      fontFamily: 'WaterBrush',
+                      fontSize: 30,
+                    ),
+                  ),
+                ],
+              ),
+
+
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        child:
+                         Padding(
+                           padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+                           child: Row(
+                             crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.phone,color: Colors.teal,),
+                              SizedBox(width: 10),
+                              Text("+90 505 316 50 96", style: TextStyle(color: Colors.teal, fontSize: 17,fontWeight: FontWeight.bold),),
+                            ],
+                        ),
+                         ),
+
+
+                      ),
+
+
+                    ],
+
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        child:
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.mail, color: Colors.teal,),
+                              SizedBox(width: 10),
+                           //   Text("+90 505 316 50 96", style: TextStyle(color: Colors.teal, fontSize: 17,fontWeight: FontWeight.bold)),
+                              Text("sinanalagoz74@gmail.com",
+                                style: TextStyle(color: Colors.teal,
+                                  fontFamily: "PressStart2P",
+                                  fontSize: 10,
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+
+
+                      ),
+
+
+                    ],
+
+                  ),
+
+
+
+
+
+                ],
+              ),
+
 
             ],
           ),
